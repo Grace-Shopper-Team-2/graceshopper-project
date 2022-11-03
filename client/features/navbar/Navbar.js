3e90422d24585aebeb21455a2c800b1b7a1889d9
Noor Mudassar <noormudassar97@gmail.com>
2022-11-03 02:36:59 -0400

    added styling to admin navbar

--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -29,41 +29,136 @@ const Navbar = () => {
 
   return (
     <div>
-      <div className="login-info"></div>
-      <nav className="nav-bar">
-        {isLoggedIn ? (
-          isAdmin ? (
-            <div className="nav">
-              {/* The navbar will show these links after you log in */}
-              <div className="nav-items">
-                <div className="search">
-                  <input
-                    type={"text"}
-                    class="search-box"
-                    placeholder="Search Magical Item"
-                  />
-                  <button class="search-button">Search</button>
+      <div className="login-info">
+        <nav className="nav-bar">
+          {isLoggedIn ? (
+            isAdmin ? (
+              <div className="login-info">
+                <div className="nav">
+                  {/* The navbar will show these links after you log in */}
                   <Link to="/home">Home</Link>
                   <Link to="/profilePage">Profile Page</Link>
-                  <Link to="/products">Shops</Link>
-                  <Link to="/adminPage">Admin Page</Link>
-                  <button
-                    type="button"
-                    class="btn btn-light"
-                    onClick={logoutAndRedirectHome}
-                  >
-                    Logout
-                  </button>
-                  <h6>Logged in as: {username}</h6>
+                  <Link to="/adminPage">Admin</Link>
+                  <div class="dropdown">
+                    <button
+                      class="btn dropdown-toggle"
+                      type="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      Shops
+                    </button>
+                    <ul class="dropdown-menu">
+                      <li>
+                        <Link to="/products">Ollivanders </Link>
+                      </li>
+                      <li>
+                        <a class="dropdown-item disabled">
+                          COMING SOON! - Weasleys’ Wizard Wheezes
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item disabled">
+                          COMING SOON! - Quality Quidditch Supplies
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                  <Link to="/cart">
+                    {" "}
+                    {`Cart (${totalNumOfItemsInCart(cart.cartItems)})`}{" "}
+                  </Link>
+                  <div className="nav-items">
+                    <div class="search">
+                      <input
+                        type={"text"}
+                        class="search-box"
+                        placeholder="Search Magical Item"
+                      />
+                      <button class="search-button">Search</button>
+                    </div>
+                  </div>
+                  <div className="login-text">
+                    <p> Logged in as Admin</p>
+                    <br />
+                    <button
+                      className="login-button"
+                      type="button"
+                      class="btn btn-outline-light btn-sm rounded mr-2"
+                      onClick={logoutAndRedirectHome}
+                    >
+                      Logout
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
+            ) : (
+              <div className="login-info">
+                <div className="nav">
+                  {/* The navbar will show these links after you log in */}
+                  <Link to="/home">Home</Link>
+                  <Link to="/profilePage">Profile Page</Link>
+                  <div class="dropdown">
+                    <button
+                      class="btn dropdown-toggle"
+                      type="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      Shops
+                    </button>
+                    <ul class="dropdown-menu">
+                      <li>
+                        <Link to="/products">Ollivanders </Link>
+                      </li>
+                      <li>
+                        <a class="dropdown-item disabled">
+                          COMING SOON! - Weasleys’ Wizard Wheezes
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item disabled">
+                          COMING SOON! - Quality Quidditch Supplies
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                  <Link to="/cart">
+                    {" "}
+                    {`Cart (${totalNumOfItemsInCart(cart.cartItems)})`}{" "}
+                  </Link>
+                  <div className="nav-items">
+                    <div class="search">
+                      <input
+                        type={"text"}
+                        class="search-box"
+                        placeholder="Search Magical Item"
+                      />
+                      <button class="search-button">Search</button>
+                    </div>
+                  </div>
+                  <div className="login-text">
+                    <p> Logged in as: {username}</p>
+                    <br />
+                    <button
+                      className="login-button"
+                      type="button"
+                      class="btn btn-outline-light btn-sm rounded mr-2"
+                      onClick={logoutAndRedirectHome}
+                    >
+                      Logout
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )
           ) : (
             <div className="login-info">
               <div className="nav">
-                {/* The navbar will show these links after you log in */}
+                {/* The navbar will show these links before you log in */}
                 <Link to="/home">Home</Link>
-                <Link to="/profilePage">Profile Page</Link>
+                <Link to="/login">Login</Link>
+                <Link to="/signup">Sign Up</Link>
                 <div class="dropdown">
                   <button
                     class="btn dropdown-toggle"
@@ -103,72 +198,12 @@ const Navbar = () => {
                     <button class="search-button">Search</button>
                   </div>
                 </div>
-                <div className="login-text">
-                  <p> Logged in as: {username}</p>
-                  <br />
-                  <button
-                    className="login-button"
-                    type="button"
-                    class="btn btn-outline-light btn-sm rounded mr-2"
-                    onClick={logoutAndRedirectHome}
-                  >
-                    Logout
-                  </button>
-                </div>
               </div>
             </div>
-          )
-        ) : (
-          <div className="login-info">
-            <div className="nav">
-              {/* The navbar will show these links before you log in */}
-              <Link to="/home">Home</Link>
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Sign Up</Link>
-              <div class="dropdown">
-                <button
-                  class="btn dropdown-toggle"
-                  type="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Shops
-                </button>
-                <ul class="dropdown-menu">
-                  <li>
-                    <Link to="/products">Ollivanders </Link>
-                  </li>
-                  <li>
-                    <a class="dropdown-item disabled">
-                      COMING SOON! - Weasleys’ Wizard Wheezes
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item disabled">
-                      COMING SOON! - Quality Quidditch Supplies
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <Link to="/cart">
-                {" "}
-                {`Cart (${totalNumOfItemsInCart(cart.cartItems)})`}{" "}
-              </Link>
-              <div className="nav-items">
-                <div class="search">
-                  <input
-                    type={"text"}
-                    class="search-box"
-                    placeholder="Search Magical Item"
-                  />
-                  <button class="search-button">Search</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        )}
-      </nav>
-      <hr />
+          )}
+        </nav>
+        <hr />
+      </div>
     </div>
   );
 };
